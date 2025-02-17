0a8204489e3bcbf2e07f5e26d276f9aa18616042
Aditi392 <195753387+Aditi392@users.noreply.github.com>
2025-02-18 01:36:45 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -15,7 +15,7 @@ int main()
         printf("%d",a*b);
         break;
         case '/':
-        printf("%d", a/b)
+        printf("%d", a/b);
         if(a%2!=0)
        { printf("%d",a/b);}
         else 
