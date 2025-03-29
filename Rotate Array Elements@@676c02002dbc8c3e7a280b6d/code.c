306f4f16261164cd43d4f862490f47e47740ff53
Aditi392 <tanisharani447@gmail.com>
2025-03-29 16:12:42 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,50 +1,20 @@
-#include <stdio.h>
-
-// Function to rotate array to the right by one position
-void rotateArray(int arr[], int n, int k) {
-    k = k % n; // Handle cases where k > n
-    int temp[k];
-    
-    // Store last k elements in a temp array
-    for (int i = 0; i < k; i++) {
-        temp[i] = arr[n - k + i];
-    }
-    
-    // Shift elements to the right
-    for (int i = n - 1; i >= k; i--) {
-        arr[i] = arr[i - k];
-    }
-    
-    // Copy back the k elements to the front
-    for (int i = 0; i < k; i++) {
-        arr[i] = temp[i];
+#include<stdio.h>
+void reverse(int arr[])
+{
+    int i;
+    int j;
+    while(i<=j)
+    int temp = arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    i++;
+    j++;
+}
+int main()
+{
+    int arr={};
+    reverse(arr);
+    for(int i=0;i<=;i++){
+        printf("%d\n",arr[i]);
     }
 }
-
-// Function to print an array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int k;
-    
-    printf("" );
-    scanf("%d", &k);
-    
-    printf("\n");
-    printArray(arr, n);
-    
-    rotateArray(arr, n, k);
-    
-    printf("\n");
-    printArray(arr, n);
-    
-    return 0;
-}
-
